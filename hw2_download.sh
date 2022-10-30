7bed408e3e1e34c29e25af373cefc56e66be3122
JustinHuang1111 <73697339+JustinHuang1111@users.noreply.github.com>
2022-10-30 13:27:30 +0800

    hw2 modify download data

--- hw2_download.sh
+++ hw2_download.sh
@@ -5,3 +5,9 @@ mkdir ./p3/ckpt
 
 wget -O ./p3/ckpt/test_aug_model_210.ckpt https://www.dropbox.com/s/g1d7f9ycy3okn94/test_aug_model_210.ckpt?dl=0
 wget -O ./p3/ckpt/usps_4_model_46.ckpt https://www.dropbox.com/s/1gjmn64a1f2jmrn/usps_4_model_46.ckpt?dl=0
+
+wget -O ./p2/ckpt/model_99_4.pth https://www.dropbox.com/s/c9aw6iuy2945d25/model_99_4.pth?dl=0
+
+wget -O ./p1/ckpt/1028-0847_dcgan.pth https://www.dropbox.com/s/6k8jo6qq5pi80wl/1028-0847_dcgan.pth?dl=0
+wget -O ./p1/ckpt/1029-0209_dcgan.pth https://www.dropbox.com/s/wlq9vkkykwy5eae/1029-0209_dcgan.pth?dl=0
+
