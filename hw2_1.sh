d84452f864e887bbead5873c1c10c5d5607e51c5
JustinHuang1111 <73697339+JustinHuang1111@users.noreply.github.com>
2022-10-31 17:00:52 +0800

    hw2 modify bash

--- hw2_1.sh
+++ hw2_1.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # TODO - run your inference Python3 code
-mkdir $1
 python ./p1/inference.py --model1 ./p1/ckpt/1029-0209_dcgan.pth --model2 ./p1/ckpt/1028-0847_dcgan.pth --seed 99 --outpath $1
